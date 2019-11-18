2f8d46e3fc36a300a6053910d335385d9afaf02a
ohanaomer1@gmail.com <ohanaomer1@gmail.com>
2019-11-18 22:09:15 +0100

    more excexercise

--- array/expense-tracker.js
+++ array/expense-tracker.js
@@ -1,10 +1,31 @@
 const account = {
     name: 'Omer Ohana',
-    expenses: []
+    expenses: [],
+    //addExpense => descrition, amount
+    addExpense: function (descrip, amount) {
+        //this will accsses the obj
+        //push() pass property to the array
+        //expenses = the array
+        this.expenses.push({
+            //new properties will take the arguments. 
+            description: descrip,
+            amount: amount
+        });
+    },
+    //claculate the amount: 
+    getAccountSummary: function () {
+        let totalAmount = 0;
+        //pass a callback function and allow to accsses indevidual item
+        this.expenses.forEach(function (expens) {
+            totalAmount = totalAmount + expens.amount
+        });
+        return `${this.name} has ${totalAmount} in expenses.`;
+    }
 }
 
 //Expenses => description, amount
 //addExpense => descrition, amount
+
 //getAccountSummary => total up all expenses -> {account name} has {amount sum} in expenses.
 
 account.addExpense('Rent', 950);
